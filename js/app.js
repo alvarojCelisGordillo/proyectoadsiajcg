11bd1e7566819bd331569c2716567b3aa7129d9b
Alvaro <celistony@yahoo.com>
2021-08-08 02:14:21 -0500

    actualice la logica

--- js/app.js
+++ js/app.js
@@ -1,6 +1,38 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: "Hola Mundo por 100000 vez"
-    }
-})
+        number_1: 0,
+        number_2: 0,
+        response: null
+
+    },
+
+    methods: {
+
+        sum() {
+            this.response = this.number_1 + this.number_2;
+
+        },
+
+        subtract() {
+            this.response = this.number_1 - this.number_2;
+        },
+
+        multiply() {
+            this.response = this.number_1 * this.number_2;
+        },
+
+        divide() {
+
+            if (this.number_2 == 0) {
+                this.response = "No se puede dividir por 0 crack!"
+            } else {
+
+                this.response = this.number_1 / this.number_2;
+            }
+        }
+
+
+
+    },
+});
